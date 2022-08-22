7c8a63e77ba1176ac736556bf6267b4623cbb41a
aexisdev <111805750+aexisdev@users.noreply.github.com>
2022-08-22 17:10:25 -0400

    Add files via upload

---
+++ eccomerceAcademlo/assets/index.js
@@ -0,0 +1,11 @@
+    // *LOGO DEL CARRITO 
+const carrito = document.querySelector('#card');
+    // *CAFES
+const  listaCafes = document.querySelector('');    
+    // *LISTA CAFES 
+
+    // *VACIAR CARRITO 
+function escuchandoCarrito(e){
+    console.log('click')
+}
+carrito.addEventListener('click' , escuchandoCarrito)
